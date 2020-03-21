73fef43b1e864c9e022821b7c5601475c52df50c
Erik Pehrers <erik.pehrers@jetshop.se>
2020-03-22 00:33:06 +0100

    Remove static file serve

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,15 +1,12 @@
 import * as express from 'express'
 import * as http from 'http'
 import * as socket from 'socket.io'
-import * as path from 'path'
 import * as dotenv from 'dotenv'
 import { log } from './utils/log'
 
 dotenv.config()
 
 const PORT = process.env.PORT || 4000
-const WEB_ROOT = path.join(__dirname, '../../web')
-const ENV = 'production'
 
 async function startServer() {
     const app = express();
@@ -17,14 +14,8 @@ async function startServer() {
     const io = socket(server);
     let connections = 0;
 
-    console.log(connections)
-
-    if (process.env.NODE_ENV === ENV) {
-        app.use(express.static(path.join(WEB_ROOT, 'build')));
-    }
-    app.get('/', (_, res) => res.sendFile(WEB_ROOT))
-
     io.on('connection', socket => {
+        console.log({ connections })
         log('CONNECTED')
         connections++
         io.emit('init', { connections })
